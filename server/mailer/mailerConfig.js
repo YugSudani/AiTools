e262c55e6c1ec4ef410e3567c9ee15e91a9088b3
YugSudani <yugsudani88@gmail.com>
2025-10-14 17:47:39 +0530

    testing new mailer server2

--- server/mailer/mailerConfig.js
+++ server/mailer/mailerConfig.js
@@ -1,9 +1,17 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-export const transporter = nodemailer.createTransport({
-  service: 'SendGrid',
+// export const transporter = nodemailer.createTransport({
+//   service: 'SendGrid',
+//   auth: {
+//     user: 'apikey', // literally the string 'apikey'
+//     pass: process.env.SENDGRID_API_KEY
+//   }
+// });
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+
+export const transporter = nodemailer.createTransport(sgTransport({
   auth: {
-    user: 'apikey', // literally the string 'apikey'
-    pass: process.env.SENDGRID_API_KEY
+    api_key: process.env.SENDGRID_API_KEY
   }
-});
+}));
