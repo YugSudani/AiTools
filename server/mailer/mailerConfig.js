ebf8a74207f62833d3010b74f724802c78e6fb80
YugSudani <yugsudani88@gmail.com>
2025-10-14 14:05:24 +0530

    otp config bug fix2

--- server/mailer/mailerConfig.js
+++ server/mailer/mailerConfig.js
@@ -1,10 +1,17 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.OTP_Email_ID,
-        pass:process.env.OTP_Email_pwd
-    }
+    // service:'gmail',
+    // auth:{
+    //     user:process.env.OTP_Email_ID,
+    //     pass:process.env.OTP_Email_pwd
+    // }
+    host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.OTP_EMAIL,
+    pass: process.env.OTP_EMAIL_PASS
+  }
 })
 
